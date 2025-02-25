a8b43f47bc612b5da3edb1df741c6cca66580c92
andrey-stekov <andrey.stekov@gmail.com>
2025-02-25 12:17:23 +0100

    Initial

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ClassicComponent from "./ClassicComponent"
+
+function App() {
+  return (
+    <>
+      <ClassicComponent initialCount={0} secondsNumber={3} />
+    </>
+  )
+}
+
+export default App
